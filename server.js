0d259ce59d062491778040646ccdbde34db2e8a6
unknown <aadeye302@caledonian.ac.uk>
2025-09-25 22:28:33 +0100

    git com

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const app = require('./index')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
+const port = process.env.PORT || 3000
 
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
 mongoose.connect(DB, {useNewUrlParser: true,useUnifiedTopology: true}).then(
     ()=>{console.log('DB connection succesful')}) 
 
-app.listen(3000, "127.0.0.1", ()=>{
-    console.log('Listening on port 3000')
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
 })
